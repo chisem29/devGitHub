389a0f4c686926e741ef995c64bd75887547b222
chisem29 <tonyfhg1000@gmail.com>
2023-05-24 18:22:38 +0300

    xz

---
+++ ReactApps/useWebpack/devEnding/src/app.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const App : FC = () => {
+  return (
+    <>
+      <input placeholder="asdasdasdasddsa"></input>
+    </>
+  )
+}
+
+export default App
---
+++ ReactApps/useWebpack/devEnding/webpack.config.js
@@ -0,0 +1,57 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+
+module.exports = {
+  mode: 'none',
+  entry: {
+    app: path.join(__dirname, 'src', 'index.tsx')
+  },
+  target: 'web',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  devServer: {
+    allowedHosts: 'all',
+    port : 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                  ],
+                ],
+              },
+            },
+          }
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean : true
+  }
+}
