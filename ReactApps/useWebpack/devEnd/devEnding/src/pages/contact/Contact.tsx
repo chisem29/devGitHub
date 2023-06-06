00b42499db002aa5230c9ffa4065bd54b9b627b7
chisem29 <tonyfhg1000@gmail.com>
2023-06-06 17:23:46 +0300

    dasd

--- ReactApps/useWebpack/devEnd/devEnding/src/pages/contact/Contact.tsx
+++ ReactApps/useWebpack/devEnd/devEnding/src/pages/contact/Contact.tsx
@@ -5,6 +5,8 @@ import { useParams } from "react-router-dom"
 import { useGetContactByIdQuery } from "../../store/services/api"
 import HandlerStatus from "../../components/UI/handlerStatus/HandlerStatus"
 
+//https://nicepage.com/css-templates/preview/three-popular-services-510987?device=desktop
+
 const Contact : FC = () => {
 
   const { id } = useParams()
